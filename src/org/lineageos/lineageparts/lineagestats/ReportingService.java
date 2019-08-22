3b11b2de9babdd6ceb2d19716e909567787b3250
Pranav Vashi <neobuddy89@gmail.com>
2019-08-22 22:51:55 +0530

    Add initial lluvia stats support
    Signed-off-by: Ram9199 <sriramscience7@gmail.com>
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>

--- src/org/lineageos/lineageparts/lineagestats/ReportingService.java
+++ src/org/lineageos/lineageparts/lineagestats/ReportingService.java
@@ -27,7 +27,7 @@ import android.os.PersistableBundle;
 import android.util.Log;
 
 public class ReportingService extends IntentService {
-    /* package */ static final String TAG = "LineageStats";
+    /* package */ static final String TAG = "lluviaStats";
     private static final boolean DEBUG = Log.isLoggable(TAG, Log.DEBUG);
 
     public ReportingService() {
@@ -40,40 +40,46 @@ public class ReportingService extends IntentService {
 
         String deviceId = Utilities.getUniqueID(getApplicationContext());
         String deviceName = Utilities.getDevice();
-        String deviceVersion = Utilities.getModVersion();
+        String deviceCrVersion = Utilities.getModVersion();
+        String deviceBuildDate = Utilities.getBuildDate();
+        String deviceAndroidVersion = Utilities.getAndroidVersion();
+        String deviceTag = Utilities.getTag();
         String deviceCountry = Utilities.getCountryCode(getApplicationContext());
         String deviceCarrier = Utilities.getCarrier(getApplicationContext());
         String deviceCarrierId = Utilities.getCarrierId(getApplicationContext());
 
-        final int lineageOldJobId = AnonymousStats.getLastJobId(getApplicationContext());
-        final int lineageOrgJobId = AnonymousStats.getNextJobId(getApplicationContext());
+        final int lluviaOldJobId = AnonymousStats.getLastJobId(getApplicationContext());
+        final int lluviaOrgJobId = AnonymousStats.getNextJobId(getApplicationContext());
 
-        if (DEBUG) Log.d(TAG, "scheduling job id: " + lineageOrgJobId);
+        if (DEBUG) Log.d(TAG, "scheduling job id: " + lluviaOrgJobId);
 
-        PersistableBundle lineageBundle = new PersistableBundle();
-        lineageBundle.putString(StatsUploadJobService.KEY_DEVICE_NAME, deviceName);
-        lineageBundle.putString(StatsUploadJobService.KEY_UNIQUE_ID, deviceId);
-        lineageBundle.putString(StatsUploadJobService.KEY_VERSION, deviceVersion);
-        lineageBundle.putString(StatsUploadJobService.KEY_COUNTRY, deviceCountry);
-        lineageBundle.putString(StatsUploadJobService.KEY_CARRIER, deviceCarrier);
-        lineageBundle.putString(StatsUploadJobService.KEY_CARRIER_ID, deviceCarrierId);
-        lineageBundle.putLong(StatsUploadJobService.KEY_TIMESTAMP, System.currentTimeMillis());
+        PersistableBundle lluviaBundle = new PersistableBundle();
+        lluviaBundle.putString(StatsUploadJobService.KEY_DEVICE_NAME, deviceName);
+        lluviaBundle.putString(StatsUploadJobService.KEY_UNIQUE_ID, deviceId);
+        lluviaBundle.putString(StatsUploadJobService.KEY_CR_VERSION, deviceCrVersion);
+        lluviaBundle.putString(StatsUploadJobService.KEY_BUILD_DATE, deviceBuildDate);
+        lluviaBundle.putString(StatsUploadJobService.KEY_ANDROID_VERSION, deviceAndroidVersion);
+        lluviaBundle.putString(StatsUploadJobService.KEY_TAG, deviceTag);
+        lluviaBundle.putString(StatsUploadJobService.KEY_COUNTRY, deviceCountry);
+        lluviaBundle.putString(StatsUploadJobService.KEY_CARRIER, deviceCarrier);
+        lluviaBundle.putString(StatsUploadJobService.KEY_CARRIER_ID, deviceCarrierId);
+        lluviaBundle.putLong(StatsUploadJobService.KEY_TIMESTAMP, System.currentTimeMillis());
 
         // set job types
-        lineageBundle.putInt(StatsUploadJobService.KEY_JOB_TYPE,
-                StatsUploadJobService.JOB_TYPE_LINEAGEORG);
+        lluviaBundle.putInt(StatsUploadJobService.KEY_JOB_TYPE,
+                StatsUploadJobService.JOB_TYPE_LLUVIA);
 
-        // schedule lineage stats upload
-        js.schedule(new JobInfo.Builder(lineageOrgJobId, new ComponentName(getPackageName(),
+        // schedule lluvia stats upload
+        js.schedule(new JobInfo.Builder(lluviaOrgJobId, new ComponentName(getPackageName(),
                 StatsUploadJobService.class.getName()))
                 .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
                 .setMinimumLatency(1000)
-                .setExtras(lineageBundle)
+                .setExtras(lluviaBundle)
                 .setPersisted(true)
                 .build());
 
         // cancel old job in case it didn't run yet
-        js.cancel(lineageOldJobId);
+        js.cancel(lluviaOldJobId);
 
         // reschedule
         AnonymousStats.updateLastSynced(this);
